9261bd29e4dcc00beb2537d4bcc7c5eeb0b67431
nagruy <uy.nagr@gmail.com>
2017-07-12 21:50:35 -0300

    Add files via upload

---
+++ MwSSE.hooks.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Hooks for MwSSE extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+class MwSSEHooks {
+
+    public static function onlogBadLogin( $ret, $user, $userName ){
+
+		// Database credentials (LocalSettings.php)
+		global $wgDBserver;
+		global $wgDBuser;
+		global $wgDBname;
+		global $wgDBpassword;
+
+		// Data to send email (LocalSettings.php)
+		global $wgMetaNamespace;
+		global $wgEmergencyContact;
+
+        	if( $ret->status === "FAIL" ) {
+	        	$wgEmailSubject = "$wgMetaNamespace login ERROR";
+		} else {
+			$wgEmailSubject = "$wgMetaNamespace login OK";
+		}
+	
+	     	$conn = new mysqli($wgDBserver, $wgDBuser, $wgDBpassword, $wgDBname);
+	   
+		$sql = "SELECT user_email FROM user WHERE lower(user_name)=lower('$userName')";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+
+		$email = $row['user_email'];
+
+		error_log("[MwSSE - Extension] $userName: $ret->status email FROM: $wgEmergencyContact TO: $email");
+
+		$body = "Login attempt! $userName LOGIN";
+
+		$headers = "From:".$wgEmergencyContact."\r\n";
+		$mailstatus = mail($email, $wgEmailSubject, $body, $headers);
+		return false;
+    }
+}
